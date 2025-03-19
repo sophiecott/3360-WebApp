26e6b0f442698b75c173b73d7b5f4f4600ba73eb
sophiecott <97978942+sophiecott@users.noreply.github.com>
2025-03-19 17:54:10 -0600

    Add project files.

---
+++ Tutorial/Areas/Help/Controllers/InfoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial.Areas.Help.Controllers
+{
+    [Area("Help")]
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
